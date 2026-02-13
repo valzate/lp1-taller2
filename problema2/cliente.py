61530e6ddaa4975977d1d085b27370bdfcb17f66
valzate <valzate223@gmail.com>
2026-02-12 20:04:54 -0500

    se resolvio el problema 2

--- problema2/cliente.py
+++ problema2/cliente.py
@@ -7,26 +7,37 @@ Objetivo: Crear un cliente TCP que envíe un mensaje al servidor y reciba la mis
 import socket
 
 # TODO: Definir la dirección y puerto del servidor
+HOST = "localhost"
+PORT = 9001
 
-# Solicitar mensaje al usuario por consola
-message = input("Mensaje: ")
+while True:
 
-# TODO: Crear un socket TCP/IP
-# AF_INET: socket de familia IPv4
-# SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+    # TODO: Crear un socket TCP/IP
+    # AF_INET: socket de familia IPv4
+    # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: Conectar el socket al servidor en la dirección y puerto especificados
+    # TODO: Conectar el socket al servidor en la dirección y puerto especificados
+    conn.connect((HOST, PORT))
+    
+    # Solicitar mensaje al usuario por consola
+    message = input("Mensaje: ")
 
-# Mostrar mensaje que se va a enviar
-print(f"Mensaje '{message}' enviado.")
+    # Mostrar mensaje que se va a enviar
+    print(f"Mensaje '{message}' enviado.")
 
-# TODO: Codificar el mensaje a bytes y enviarlo al servidor
+    # TODO: Codificar el mensaje a bytes y enviarlo al servidor
+    conn.sendall(message.encode())
 # sendall() asegura que todos los datos sean enviados
 
-# TODO: Recibir datos del servidor (hasta 1024 bytes)
+    # TODO: Recibir datos del servidor (hasta 1024 bytes)
+    data = conn.recv(1024)
+    # Decodificar e imprimir los datos recibidos
+    print("Mensaje recibido: ", data.decode())
 
-# Decodificar e imprimir los datos recibidos
-print("Mensaje recibido: ", data.decode())
+    # TODO: Cerrar la conexión con el servidor
+    conn.close()
 
-# TODO: Cerrar la conexión con el servidor
+    if message == "adios":
+        break
 
--- problema2/servidor.py
+++ problema2/servidor.py
@@ -7,14 +7,19 @@ Objetivo: Crear un servidor TCP que devuelva exactamente lo que recibe del clien
 import socket
 
 # TODO: Definir la dirección y puerto del servidor
+HOST = "localhost"
+PORT = 9001
 
 # TODO: Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # TODO: Enlazar el socket a la dirección y puerto especificados
-
+servidor.bind((HOST,PORT))
 # TODO: Poner el socket en modo escucha
+servidor.listen()
+
 # El parámetro define el número máximo de conexiones en cola
 
 # Bucle infinito para manejar múltiples conexiones (una a la vez)
@@ -26,19 +31,22 @@ while True:
     # accept() bloquea hasta que llega una conexión
     # conn: nuevo socket para comunicarse con el cliente
     # addr: dirección y puerto del cliente
+    conn, addr = servidor.accept()
     
-    print(f"Conexión realizada por {addr}")
+    print(f"Conexión realizada ")
 
     # TODO: Recibir datos del cliente (hasta 1024 bytes)
+    data = conn.recv(1024)
     
     # Si no se reciben datos, salir del bucle
     if not data:
         break
 
     # Mostrar los datos recibidos (en formato bytes)
-    print("Datos recibidos:", data)
+    print("Datos recibidos: ", data)
     
     # TODO: Enviar los mismos datos de vuelta al cliente (echo)
+    conn.sendall(b"HECHO!")
     
     # TODO: Cerrar la conexión con el cliente actual
-
+    conn.close()
