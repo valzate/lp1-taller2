826c72f759d39d4daf1d0cb7c8dc904e38a7d633
valzate <valzate223@gmail.com>
2026-02-12 19:03:24 -0500

    se resolvio el problema 1

--- problema1/cliente.py
+++ problema1/cliente.py
@@ -5,19 +5,24 @@ Objetivo: Crear un cliente TCP que se conecte a un servidor e intercambie mensaj
 """
 
 import socket
+HOST = "localhost"
+PORT = 9000
 
 # TODO: Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # TODO: Conectar el socket al servidor en la dirección y puerto especificados
+conn.connect((HOST, PORT))
 
 # TODO: Enviar datos al servidor (convertidos a bytes)
+conn.sendall(b"Mundo!")
 # sendall() asegura que todos los datos sean enviados
 
 # TODO: Recibir datos del servidor (hasta 1024 bytes)
-
+respuesta = conn.recv(1024)
 # TODO: Decodificar e imprimir los datos recibidos
-
+print(f"Respuesta: {respuesta}")
 # TODO: Cerrar la conexión con el servidor
-
+conn.close()
--- problema1/servidor.py
+++ problema1/servidor.py
@@ -7,29 +7,36 @@ Objetivo: Crear un servidor TCP que acepte una conexión y intercambie mensajes 
 import socket
 
 # TODO: Definir la dirección y puerto del servidor
+HOST = "localhost"
+PORT = 9000
 
 # TODO: Crear un socket TCP/IP
 # AF_INET: socket de familia IPv4
 # SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # TODO: Enlazar el socket a la dirección y puerto especificados
+servidor.bind((HOST, PORT))
 
 # TODO: Poner el socket en modo escucha
-# El parámetro define el número máximo de conexiones en cola
-
+servidor.listen() #pone al servidor en espera de conexiones
 print("Servidor a la espera de conexiones ...")
 
+
+# El parámetro define el número máximo de conexiones en cola
 # TODO: Aceptar una conexión entrante
+conn, direccion = servidor.accept()
 # accept() bloquea hasta que llega una conexión
 # conn: nuevo socket para comunicarse con el cliente
 # addr: dirección y puerto del cliente
 
-print(f"Conexión realizada por {addr}")
+print(f"Conexión realizada por {HOST}")
 
 # TODO: Recibir datos del cliente (hasta 1024 bytes)
- 
+datos = conn.recv(1024)
 # TODO: Enviar respuesta al cliente (convertida a bytes)
+conn.sendall(b"Hola! "+ datos) #la "b" indica binario, y asi es como se manda  
 # sendall() asegura que todos los datos sean enviados
 
 # TODO: Cerrar la conexión con el cliente
-
+conn.close()
